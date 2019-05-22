560219ac47ecac7c0f01580c996cf2b596a07240
Tijana Gava <tijana.gava@hotmail.com>
2019-05-22 15:39:21 +0200

    modified salat-detail

--- src/app/salads/salad-detail/salad-detail.component.ts
+++ src/app/salads/salad-detail/salad-detail.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SaladsService} from '../salads.service';
+import {Salad} from '../salad.model';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-salad-detail',
-  templateUrl: './salad-detail.component.html',
-  styleUrls: ['./salad-detail.component.scss']
+    selector: 'app-salad-detail',
+    templateUrl: './salad-detail.component.html',
+    styleUrls: ['./salad-detail.component.scss']
 })
 export class SaladDetailComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    // ***** Fields ***** //
 
+    // Salad Data
+    salad: Salad = {};
+
+
+    // ***** Lifecycle ***** //
+
+    constructor(private saladsService: SaladsService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.saladsService.fetchSaladData(this.route.snapshot.params['id']).subscribe(res => this.salad = res);
+    }
 }
